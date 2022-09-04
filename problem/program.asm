26ccef3b99ebc594a1d3389f689efbd5faf549d3
SamuelElHG <samuel.hernandezg@upb.edu.co>
2022-09-04 13:32:58 -0500

    debería estar bueno

--- problem/program.asm
+++ problem/program.asm
@@ -3,9 +3,9 @@
 // samuel.hernandezg@upb.edu.co
 
 (INICIO)
-            @16384
+            @16400 //16384 
             D=A
-            @PInicio//Tenemos un inicio para los contadores
+            @PInicio//Tenemos un inicio para los contadores vertical
             M=D
             @24576
             D=A
@@ -21,6 +21,20 @@
             D=M
             @FinalClear//Finalizamos los pixeles contadores para clear
             M=D
+// Inicialización datos verticales finaliza
+// Inicialización datos horizontales inicia
+
+
+            @20480
+            D=A
+            @InicioClearH //Iniciamos los pixel de contadores para clear
+            M=D
+
+            @20512
+            D=A
+            @FinalClearH//Finalizamos los pixeles contadores para clear
+            M=D
+
             //Aquí empiezan las cosas del keyboard
         @84
         D=A
@@ -42,7 +56,7 @@
         D=M
         @T
         D=D-M
-        @FILL
+        @FILLV
         D;JEQ
         @INICIO
         0;JMP
@@ -70,12 +84,16 @@
         0;JMP
 
 
-(FILL)
+(FILLV)
+        @512 //valor a pintar
+        D=A
         @InicioClear
         A=M
-        M=-1
+        M=D
+        @32 //SALTO
+        D=A //SALTO
         @InicioClear
-        M=M+1
+        M=M+D //salto 512
         //comprobar clear
         @FinalClear
         D=M
@@ -84,13 +102,37 @@
         @FILL
         D;JGT
         //El fill está bueno
+        @FILLH
+        0;JMP
+
+
+(FILLH)
+        @512 //valor a pintar
+        D=A
+        @InicioClearH
+        A=M
+        M=-1
+        @InicioClearH
+        M=M+1 //salto 512
+        //comprobar clear
+        @FinalClearH
+        D=M
+        @InicioClearH
+        D=D-M
+        @FILLH
+        D;JGT
+        //El fill está bueno
         @INICIO
         0;JMP
+
 
 (END)
         @END
         0;JMP
         
+
+
+    
 
 
 
